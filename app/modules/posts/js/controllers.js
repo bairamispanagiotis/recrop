ec9016ee9499abe10690f0cd4705d19be07a7997
Panos <bairamispanagiotis@gmail.com>
2015-04-11 19:43:44 +0300

    forms new - update

--- app/modules/posts/js/controllers.js
+++ app/modules/posts/js/controllers.js
@@ -5,30 +5,9 @@ angular.module('saveCrop.posts.controllers',[]).controller
 
 	}]).controller('PostDetailsController',['$stateParams','$state', '$scope',function($stateParams,$state,$scope){
 
-	}]).controller('PostCreationController', ['$scope','$state','Post', 'flow', function($scope,$state,Post,flow){
-		$scope.yourModel.customMenu = [
-            ['bold', 'italic', 'underline', 'strikethrough', 'subscript', 'superscript'],
-            ['font'],
-            ['font-size'],
-            ['font-color', 'hilite-color'],
-            ['remove-format'],
-            ['ordered-list', 'unordered-list', 'outdent', 'indent'],
-            ['left-justify', 'center-justify', 'right-justify'],
-            ['code', 'quote', 'paragragh'],
-            ['link', 'image']
-        ];
+	}]).controller('PostCreationController', ['$scope','$state', function($scope,$state){
+		
 
-	}]).controller('PostUpdateController', ['$scope','Post','$stateParams', '$state', 'flow',function($scope,Post,$stateParams,$state,flow){
-		$scope.yourModel.customMenu = [
-            ['bold', 'italic', 'underline', 'strikethrough', 'subscript', 'superscript'],
-            ['font'],
-            ['font-size'],
-            ['font-color', 'hilite-color'],
-            ['remove-format'],
-            ['ordered-list', 'unordered-list', 'outdent', 'indent'],
-            ['left-justify', 'center-justify', 'right-justify'],
-            ['code', 'quote', 'paragragh'],
-            ['link', 'image']
-        ];
-
+	}]).controller('PostUpdateController', ['$scope','$stateParams', '$state',function($scope,$stateParams,$state,flow){
+		
 	}]);
