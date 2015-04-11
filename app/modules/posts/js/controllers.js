96f4babb9bd72aed63b5e3f7473b4bdc76bf15e4
Panos <bairamispanagiotis@gmail.com>
2015-04-11 22:58:15 +0300

    plugins clear,/new-/update refactor

--- app/modules/posts/js/controllers.js
+++ app/modules/posts/js/controllers.js
@@ -6,8 +6,40 @@ angular.module('saveCrop.posts.controllers',[]).controller
 	}]).controller('PostDetailsController',['$stateParams','$state', '$scope',function($stateParams,$state,$scope){
 
 	}]).controller('PostCreationController', ['$scope','$state', function($scope,$state){
-		
+		$scope.today = function() {
+			$scope.dt = new Date();
+		};
+		$scope.today();
 
-	}]).controller('PostUpdateController', ['$scope','$stateParams', '$state',function($scope,$stateParams,$state,flow){
-		
-	}]);
+		$scope.clear = function () {
+			$scope.dt = null;
+		};
+
+  // Disable weekend selection
+  $scope.disabled = function(date, mode) {
+  	return ( mode === 'day' && ( date.getDay() === 0 || date.getDay() === 6 ) );
+  };
+
+  $scope.toggleMin = function() {
+  	$scope.minDate = $scope.minDate ? null : new Date();
+  };
+  $scope.toggleMin();
+
+  $scope.open = function($event) {
+  	$event.preventDefault();
+  	$event.stopPropagation();
+
+  	$scope.opened = true;
+  };
+
+  $scope.dateOptions = {
+  	formatYear: 'yy',
+  	startingDay: 1
+  };
+
+  $scope.formats = ['dd-MMMM-yyyy', 'yyyy/MM/dd', 'dd.MM.yyyy', 'shortDate'];
+  $scope.format = $scope.formats[0];
+
+}]).controller('PostUpdateController', ['$scope','$stateParams', '$state',function($scope,$stateParams,$state,flow){
+	
+}]);
--- app/modules/posts/postmodule.js
+++ app/modules/posts/postmodule.js
@@ -1,4 +1,4 @@
-angular.module('saveCrop.posts', ['saveCrop.posts.controllers', 'wysiwyg.module', 'colorpicker.module']).config(['$stateProvider', '$locationProvider', function($stateProvider,$locationProvider){
+angular.module('saveCrop.posts', ['saveCrop.posts.controllers', 'wysiwyg.module', 'colorpicker.module', 'ui.bootstrap']).config(['$stateProvider', '$locationProvider', function($stateProvider,$locationProvider){
     $stateProvider.state('allPosts',{
         url:'/posts',
         templateUrl: 'modules/posts/views/posts.html',
